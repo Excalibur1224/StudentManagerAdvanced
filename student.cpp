781e2b71d7d90163fc4973d6b50d011190d01048
Excalibur1224 <bafalla@mail.csuchico.edu>
2019-12-06 10:30:21 -0800

    created new functions for grades

---
+++ student.cpp
@@ -0,0 +1,34 @@
+#include "student.h"
+
+void student::setName(string &first, string &last)
+{
+    firstName = first;
+    lastName = last;
+}
+string student::fullName()
+{
+    string fullName;
+    fullName = firstName;
+    fullName.append(" ");
+    fullName.append(lastName);
+    return fullName;
+}
+double student::getScore()
+{
+    double grade = 0;
+    double count = 0;
+    double average = 0;
+
+    for(int i=0;i<grades.size();i++)
+    {
+        grade += grades.at(i);
+        count ++;
+    }
+
+    average = grade/count;
+    return average;
+}
+void student::addGrade(int value)
+{
+    grades.push_back(value);
+}
